ef91976d5d90ebe68d09f21abdda39d657ea4331
fvaldez29 <fv.2900@hotmail.com>
2024-12-11 13:24:32 -0400

    Header section and main section included

--- src/routers/viewRoutes.js
+++ src/routers/viewRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const routerView = Router()
 
-routerView.get('/home', (req, res) => {
+routerView.get('/', (req, res) => {
     res.render('index', {title: 'Portfalio Web', user: req.user, layouts: 'layouts/main'})
 })
 
