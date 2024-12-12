aa2374fe68e02b7d3f504e3b1ffdd021073b8d55
fvaldez29 <fv.2900@hotmail.com>
2024-12-12 18:55:23 -0400

    some style changed

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,44 +1,58 @@
 import 'dotenv/config'
 import axios from 'axios'
 
-export const getProjectController = async(req, res) => {
-    try {
+export const getProjectController = async (req, res) => {
+  try {
+    const favoriteProjects = [
+      'ITBIS-Calculator',
+      'Menu-de-comida-INFOTEP',
+      'API-Gestion-de-tareas-API',
+      'Chatbox',
+      'Find-hat-terminal-',
+      'Sleep-Debt-Calculator'
+    ]
 
-        const favoriteProjects = [
-            'ITBIS-Calculator',
-            'Menu-de-comida-INFOTEP',
-            'API-Gestion-de-tareas-API',
-            'Chatbox',
-            'Find-hat-terminal-',
-            'Portafolio'
-        ]
+    const response = await axios.get(`https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`, {
+      headers: process.env.GITHUB_TOKEN ? { Authorization: `token ${process.env.GITHUB_TOKEN}` } : {},
+      params: {
+        per_page: 100, 
+        page: 1, 
+      },
+    })
 
-        const response = await axios.get(`https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`, {
-            headers: process.env.GITHUB_TOKEN ? { Authorization: `token ${process.env.GITHUB_TOKEN} `} : {},
-        })
-
-        const repos = response.data
+  
+    const repos = await Promise.all(
+      response.data
         .filter(repo => favoriteProjects.includes(repo.name))
-        .map(repo => ({
+        .map(async (repo) => {
+          
+          const languagesResponse = await axios.get(repo.languages_url, {
+            headers: process.env.GITHUB_TOKEN ? { Authorization: `token ${process.env.GITHUB_TOKEN}` } : {},
+          })
+
+
+          const languages = Object.keys(languagesResponse.data)
+
+          return {
             name: repo.name,
             description: repo.description,
             url: repo.html_url,
             language: repo.language,
             stars: repo.stargazers_count,
-        }));
-
-        console.log(repos)
-        console.log(process.env.GITHUB_USERNAME)
-        res.render('index', {
-            title: 'Portfalio Web',
-            repos,
-            user: req.user,
-            layouts: 'layouts/main'
+            technologies: languages.join(', '),
+          }
         })
+    )
 
-
-    } catch (error) {
-        console.log('Error fetching repositories: ', error.message)
-        res.status(500).json({ error: 'Error fetching repositories' })
-    }
+    console.log(repos)
+    res.render('index', {
+      title: 'Portafolio Web',
+      repos,
+      user: req.user,
+      layouts: 'layouts/main',
+    })
+  } catch (error) {
+    console.log('Error fetching repositories: ', error.message)
+    res.status(500).json({ error: 'Error fetching repositories' })
+  }
 }
